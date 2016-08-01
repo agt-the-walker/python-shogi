6a28a662b3456c63fb9fb73a6fac3622bcb441dc
Jerome Zago <jerome.zago@gmail.com>
2016-08-01 19:48:41 +0200

    Complete coverage of is_checkmate() and is_stalemate()

--- tests/board_test.py
+++ tests/board_test.py
@@ -30,7 +30,10 @@ class BoardTestCase(unittest.TestCase):
         self.assertEqual(str(board_none), str(board_sfen))
         self.assertEqual(repr(board_none), repr(board_sfen))
         self.assertEqual(board_none.turn, shogi.BLACK)
+
         self.assertFalse(board_sfen.is_game_over())
+        self.assertFalse(board_sfen.is_checkmate())
+        self.assertFalse(board_sfen.is_stalemate())
 
     def test_checkmate(self):
         # test basic checkmate for both sides
